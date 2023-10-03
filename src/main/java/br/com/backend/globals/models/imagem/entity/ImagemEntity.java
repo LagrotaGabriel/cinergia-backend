f1feaa6fad338c9e3ca060235242a776141ff439
Gabriel <gabriellagrota23@gmail.com>
2023-10-03 15:02:57 -0300

    Refac: Mapeamento da entidade ImagemEntity reestruturado

--- src/main/java/br/com/backend/globals/models/imagem/entity/ImagemEntity.java
+++ src/main/java/br/com/backend/globals/models/imagem/entity/ImagemEntity.java
@@ -2,29 +2,44 @@ package br.com.backend.globals.models.imagem.entity;
 
 import br.com.backend.globals.models.imagem.enums.TipoImagemEnum;
 import lombok.*;
+import org.hibernate.annotations.Comment;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.util.UUID;
 
+@Data
 @Entity
 @Builder
 @ToString
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tb_imagem")
+@Table(name = "TB_SBS_IMAGEM")
 public class ImagemEntity {
 
     @Id
+    @Comment("Chave primária da imagem - UUID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(table = "TB_SBS_IMAGEM", name = "COD_IMAGEM_IMG", nullable = false, updatable = false, length = 36)
+    private UUID uuid;
+
+    @Comment("Nome do arquivo")
+    @Column(table = "TB_SBS_IMAGEM", name = "STR_NOME_IMG", nullable = false, length = 70)
     private String nome;
+
+    @Comment("Tamanho da imagem")
+    @Column(table = "TB_SBS_IMAGEM", name = "LNG_TAMANHO_IMG", nullable = false)
     private Long tamanho;
+
     @Enumerated(EnumType.STRING)
+    @Comment("Tipo do arquivo da imagem: 0 - Jpeg, 1 - Png")
+    @Column(table = "TB_SBS_IMAGEM", name = "ENM_TIPO_IMG", nullable = false)
     private TipoImagemEnum tipo;
+
     @Lob
+    @Comment("Arquivo de imagem")
     @Type(type = "org.hibernate.type.ImageType")
+    @Column(table = "TB_SBS_IMAGEM", name = "ARQ_ARQUIVO_IMG", nullable = false)
     private byte[] arquivo;
 
 }
