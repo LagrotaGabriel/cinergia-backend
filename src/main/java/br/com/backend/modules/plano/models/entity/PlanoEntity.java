5f694c2b2b4aba9a14084ed01930a04445b79c4a
Gabriel <gabriellagrota23@gmail.com>
2023-10-04 09:35:43 -0300

    Refac: Mapeamento da entidade PlanoEntity reestruturado

--- src/main/java/br/com/backend/modules/plano/models/entity/PlanoEntity.java
+++ src/main/java/br/com/backend/modules/plano/models/entity/PlanoEntity.java
@@ -1,16 +1,18 @@
 package br.com.backend.modules.plano.models.entity;
 
 import br.com.backend.globals.enums.FormaPagamentoEnum;
+import br.com.backend.modules.cliente.models.entity.ClienteEntity;
+import br.com.backend.modules.empresa.models.entity.EmpresaEntity;
 import br.com.backend.modules.pagamento.models.entity.PagamentoEntity;
+import br.com.backend.modules.plano.models.entity.id.PlanoId;
 import br.com.backend.modules.plano.models.enums.NotificacaoEnum;
 import br.com.backend.modules.plano.models.enums.PeriodicidadeEnum;
 import br.com.backend.modules.plano.models.enums.StatusPlanoEnum;
 import lombok.*;
+import org.hibernate.annotations.Comment;
 
 import javax.persistence.*;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 @Entity
 @Builder
@@ -19,48 +21,97 @@ import java.util.Set;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tb_plano")
+@IdClass(PlanoId.class)
+@Table(name = "TB_SBS_PLANO")
 public class PlanoEntity {
 
+    //TODO REVISAR MAPEAMENTO DO CLIENTE COMO OBJETO NO PLANO
+
     @Id
+    @Comment("Chave primária do plano - UUID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    @Column(nullable = false)
-    private Long idEmpresaResponsavel;
-    @Column(nullable = false)
-    private Long idClienteResponsavel;
-    @Column(unique = true)
-    private String idAsaas;
-    @Column(nullable = false)
+    @Column(table = "TB_SBS_PLANO", name = "COD_PLANO_PLN", nullable = false, updatable = false, length = 36)
+    private UUID uuid;
+
+    @Id
+    @ManyToOne
+    @EqualsAndHashCode.Include
+    @Comment("Chave primária do plano - ID da empresa ao qual o plano faz parte")
+    @JoinColumn(table = "TB_SBS_PLANO", name = "COD_EMPRESA_PLN", nullable = false, updatable = false)
+    private EmpresaEntity empresa;
+
+    @ManyToOne
+    @EqualsAndHashCode.Include
+    @Comment("ID do cliente ao qual o plano pertence")
+    @JoinColumn(table = "TB_SBS_PLANO", name = "COD_CLIENTE_PLN", nullable = false, updatable = false)
+    private ClienteEntity cliente;
+
+    @Comment("Código de identificação do plano na integradora ASAAS")
+    @Column(table = "TB_SBS_PLANO", name = "COD_ASAAS_PLN", updatable = false, nullable = false, unique = true)
+    private String asaasId;
+
+    @Comment("Data em que o cadastro do plano foi realizado")
+    @Column(table = "TB_SBS_PLANO", name = "DT_DATACADASTRO_PLN", nullable = false, updatable = false, length = 10)
     private String dataCadastro;
-    @Column(nullable = false)
+
+    @Comment("Hora em que o cadastro do plano foi realizado")
+    @Column(table = "TB_SBS_PLANO", name = "HR_HORACADASTRO_PLN", nullable = false, updatable = false, length = 18)
     private String horaCadastro;
+
+    @Comment("Data de vencimento na qual as faturas do plano serão geradas")
+    @Column(table = "TB_SBS_PLANO", name = "DT_DATAVENCIMENTO_PLN", nullable = false, length = 10)
     private String dataVencimento;
-    @Column(nullable = false)
+
+    @Comment("Data de início da primeira fatura do plano")
+    @Column(table = "TB_SBS_PLANO", name = "DT_DATAINICIO_PLN", nullable = false, length = 10)
     private String dataInicio;
-    @Column(nullable = false)
+
+    @Comment("Descrição do plano")
+    @Column(table = "TB_SBS_PLANO", name = "STR_DESCRICAO_PLN", nullable = false, length = 70)
     private String descricao;
-    @Column(nullable = false)
+
+    @Comment("Valor do plano")
+    @Column(table = "TB_SBS_PLANO", name = "MON_VALOR_PLN", nullable = false, scale = 2)
     private Double valor;
 
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @Comment("Forma de pagamento do plano: " +
+            "0 - Boleto, " +
+            "1 - Cartão de crédito, " +
+            "2 - Cartão de débito, " +
+            "3 - Pix, " +
+            "4 - Definir")
+    @Column(table = "TB_SBS_PLANO", name = "ENM_FORMAPAGAMENTO_PLN", nullable = false)
     private FormaPagamentoEnum formaPagamento;
 
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @Comment("Status atual do plano: " +
+            "0 - Ativo, " +
+            "1 - Inativo, " +
+            "2 - Removido")
+    @Column(table = "TB_SBS_PLANO", name = "ENM_STATUS_PLN", nullable = false)
     private StatusPlanoEnum statusPlano;
 
-    @Column(nullable = false)
     @Enumerated(EnumType.STRING)
+    @Comment("Periodicidade de pagamento do plano: " +
+            "0 - Semanal, " +
+            "1 - Mensal, " +
+            "2 - Semestral, " +
+            "3 - Anual")
+    @Column(table = "TB_SBS_PLANO", name = "ENM_PERIODICIDADE_PGT", nullable = false)
     private PeriodicidadeEnum periodicidade;
 
-    @ElementCollection(fetch = FetchType.EAGER)
+    @Comment("Lista de notificações do plano")
+    @Column(table = "TB_SBS_PLANO", name = "LST_NOTIFICACOES_PLN")
     @CollectionTable(name = "NOTIFICACOES")
+    @ElementCollection(fetch = FetchType.EAGER)
     protected Set<NotificacaoEnum> notificacoes = new HashSet<>();
 
-    @OneToMany(targetEntity = PagamentoEntity.class, orphanRemoval = true, cascade = CascadeType.ALL)
     @ToString.Exclude
-    private List<PagamentoEntity> pagamentos;
+    @Builder.Default
+    @Comment("Lista de pagamentos do plano")
+    @Column(table = "TB_SBS_PLANO", name = "LST_PAGAMENTOS_PLN")
+    @OneToMany(targetEntity = PagamentoEntity.class, orphanRemoval = true, cascade = CascadeType.ALL)
+    private List<PagamentoEntity> pagamentos = new ArrayList<>();
 
 }
