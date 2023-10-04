a39b06ef009d4d6cf5ba49a56af9e8be5ca73712
Gabriel <gabriellagrota23@gmail.com>
2023-10-04 08:07:31 -0300

    Refac: Mapeamento da entidade ExclusaoEntity reestruturado

--- src/main/java/br/com/backend/globals/models/exclusao/entity/ExclusaoEntity.java
+++ src/main/java/br/com/backend/globals/models/exclusao/entity/ExclusaoEntity.java
@@ -2,6 +2,9 @@ package br.com.backend.globals.models.exclusao.entity;
 
 import javax.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.Comment;
+
+import java.util.UUID;
 
 @Entity
 @Builder
@@ -10,11 +13,20 @@ import lombok.*;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "tb_exclusao")
+@Table(name = "TB_SBS_EXCLUSAO")
 public class ExclusaoEntity {
+
     @Id
+    @Comment("Chave primária da exclusão - UUID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(table = "TB_SBS_EXCLUSAO", name = "COD_EXCLUSAO_EXC", nullable = false, updatable = false, length = 36)
+    private UUID uuid;
+
+    @Comment("Data em que a exclusão foi realizada")
+    @Column(table = "TB_SBS_EXCLUSAO", name = "DT_DATACADASTRO_EXC", nullable = false, updatable = false, length = 10)
     private String dataExclusao;
+
+    @Comment("Hora em que a exclusão foi realizado")
+    @Column(table = "TB_SBS_EXCLUSAO", name = "HR_HORACADASTRO_EXC", nullable = false, updatable = false, length = 18)
     private String horaExclusao;
 }
