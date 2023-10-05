a56ccdbb0ed413bf7499b0f01982f5bbdb1ee3a8
Gabriel <gabriellagrota23@gmail.com>
2023-10-05 18:06:50 -0300

    Refac: Módulo clientes remodelado para receber o usuário principal responsável pela requisição por parâmetro, melhorando o desempenho da aplicação e abstraindo acessos ao banco de dados. Antes era necessário que fosse realizada uma requisição para que o usuário fosse localizado.

--- src/main/java/br/com/backend/modules/cliente/repository/ClienteRepository.java
+++ src/main/java/br/com/backend/modules/cliente/repository/ClienteRepository.java
@@ -2,6 +2,7 @@ package br.com.backend.modules.cliente.repository;
 
 import br.com.backend.modules.cliente.models.entity.ClienteEntity;
 import br.com.backend.globals.models.imagem.entity.ImagemEntity;
+import br.com.backend.modules.cliente.models.entity.id.ClienteId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,33 +11,30 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+public interface ClienteRepository extends JpaRepository<ClienteEntity, ClienteId> {
 
-    @Query("SELECT c FROM ClienteEntity c WHERE c.cpfCnpj = ?1 " +
-            "and c.idEmpresaResponsavel = ?2 " +
-            "and c.exclusao IS NULL")
-    Optional<ClienteEntity> buscaPorCpfCnpjIdenticoNaEmpresaDaSessaoAtual(String cpfCnpj, Long id);
+    boolean existsByEmpresaUuidAndCpfCnpj(UUID uuidEmpresaSessao, String cpfCnpj);
+    boolean existsByEmpresaUuidAndEmail(UUID uuidEmpresaSessao, String email);
 
-    @Query("SELECT c FROM ClienteEntity c WHERE c.idEmpresaResponsavel = ?1 and c.exclusao IS NULL")
-    Page<ClienteEntity> buscaPorClientes(Pageable pageable, Long id);
+    @Query("SELECT c FROM ClienteEntity c WHERE c.empresa.uuid = ?1 and c.exclusao IS NULL")
+    Page<ClienteEntity> buscaPorClientes(Pageable pageable, UUID uuidEmpresaSessao);
 
     @Query("SELECT c FROM ClienteEntity c WHERE " +
-            "upper(c.nome) LIKE ?1% and c.idEmpresaResponsavel = ?2 and c.exclusao IS NULL " +
-            "or upper(c.email) LIKE ?1% and c.idEmpresaResponsavel = ?2 and c.exclusao IS NULL " +
-            "or upper(c.cpfCnpj) LIKE ?1% and c.idEmpresaResponsavel = ?2 and c.exclusao IS NULL")
-    Page<ClienteEntity> buscaPorClientesTypeAhead(Pageable pageable, String busca, Long id);
+            "c.empresa.uuid = ?1 and upper(c.nome) LIKE ?2% and c.exclusao IS NULL " +
+            "or c.empresa.uuid = ?1 and upper(c.email) LIKE ?2% and c.exclusao IS NULL " +
+            "or c.empresa.uuid = ?1 and upper(c.cpfCnpj) LIKE ?2% and c.exclusao IS NULL")
+    Page<ClienteEntity> buscaPorClientesTypeAhead(Pageable pageable, UUID uuidEmpresaSessao, String busca);
 
-    @Query("SELECT c FROM ClienteEntity c WHERE c.id=?1 and c.idEmpresaResponsavel = ?2 and c.exclusao IS NULL")
-    Optional<ClienteEntity> buscaPorId(Long idCliente, Long idEmpresa);
+    @Query("SELECT c FROM ClienteEntity c WHERE c.empresa.uuid = ?1 and c.uuid=?2 and c.exclusao IS NULL")
+    Optional<ClienteEntity> buscaPorId(UUID uuidEmpresaSessao, UUID uuidCliente);
 
-    @Query("SELECT c FROM ClienteEntity c WHERE c.idEmpresaResponsavel = ?1 and c.exclusao IS NULL")
-    List<ClienteEntity> buscaTodos(Long id);
+    @Query("SELECT c FROM ClienteEntity c WHERE c.empresa.uuid = ?1 and c.exclusao IS NULL")
+    List<ClienteEntity> buscaTodos(UUID uuidEmpresaSessao);
 
-    @Query("SELECT c.fotoPerfil FROM ClienteEntity c WHERE c.id=?1 and c.idEmpresaResponsavel = ?2 and c.exclusao IS NULL")
-    Optional<ImagemEntity> buscaImagemPerfilPorId(Long idColaborador, Long idEmpresa);
-
-    Optional<ClienteEntity> findByAsaasId(String asaasId);
+    @Query("SELECT c.fotoPerfil FROM ClienteEntity c WHERE c.empresa.uuid = ?1 and c.uuid=?2 and c.exclusao IS NULL")
+    Optional<ImagemEntity> buscaImagemPerfilPorId(UUID uuidEmpresaSessao, UUID uuidCliente);
 
 }
