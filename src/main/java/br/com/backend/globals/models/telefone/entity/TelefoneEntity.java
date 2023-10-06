1912bb34ff00e473621ec5deca9014e0b21fd293
Gabriel <gabriellagrota23@gmail.com>
2023-10-06 09:01:27 -0300

    Feat: - Modelagem da entidade Telefone finalizada - Agora a classe TelefoneEntity tem um método que constrói um objeto do tipo TelefoneEntity através de um objeto TelefoneRequest

--- src/main/java/br/com/backend/globals/models/telefone/entity/TelefoneEntity.java
+++ src/main/java/br/com/backend/globals/models/telefone/entity/TelefoneEntity.java
@@ -1,9 +1,14 @@
 package br.com.backend.globals.models.telefone.entity;
 
+import br.com.backend.globals.models.telefone.request.TelefoneRequest;
 import lombok.*;
 import org.hibernate.annotations.Comment;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Type;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -13,20 +18,38 @@ import java.util.UUID;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "TB_SBS_TELEFONE")
+@Table(name = "tb_sbs_telefone")
 public class TelefoneEntity {
 
     @Id
+    @Type(type = "uuid-char")
+    @GeneratedValue(generator = "UUID")
     @Comment("Chave primária do telefone - UUID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "COD_TELEFONE_TEL", nullable = false, updatable = false, length = 36)
+    @Column(name = "cod_telefone_tel", nullable = false, updatable = false)
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
     private UUID uuid;
 
     @Comment("Prefixo do telefone")
-    @Column(name = "STR_PREFIXO_TEL", nullable = false, length = 2)
+    @Column(name = "str_prefixo_tel", nullable = false, length = 2)
     private String prefixo;
 
     @Comment("Número do telefone")
-    @Column(name = "STR_NUMERO_TEL", nullable = false, length = 9)
+    @Column(name = "str_numero_tel", nullable = false, length = 9)
     private String numero;
+
+    public TelefoneEntity constroiTelefoneEntity(TelefoneRequest telefoneRequest) {
+        return TelefoneEntity.builder()
+                .prefixo(telefoneRequest.getPrefixo())
+                .numero(telefoneRequest.getNumero())
+                .build();
+    }
+
+    public List<TelefoneEntity> constroiListaTelefoneEntity(List<TelefoneRequest> telefonesRequest) {
+        List<TelefoneEntity> telefonesEntity = new ArrayList<>();
+
+        telefonesRequest.forEach(telefoneRequest ->
+                telefonesEntity.add(constroiTelefoneEntity(telefoneRequest)));
+
+        return telefonesEntity;
+    }
 }
