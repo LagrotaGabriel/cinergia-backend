2a3f8f680387a7939b4579cda83eb4d6c9a3df2a
Gabriel <gabriellagrota23@gmail.com>
2023-10-06 09:32:48 -0300

    Fix: @NotEmpty corrigidos para @NotNull, já que agora são inteiros

--- src/main/java/br/com/backend/globals/models/telefone/request/TelefoneRequest.java
+++ src/main/java/br/com/backend/globals/models/telefone/request/TelefoneRequest.java
@@ -2,10 +2,7 @@ package br.com.backend.globals.models.telefone.request;
 
 import lombok.*;
 
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 
 @Data
 @Builder
@@ -14,13 +11,13 @@ import javax.validation.constraints.Pattern;
 @AllArgsConstructor
 public class TelefoneRequest {
 
-    @NotEmpty(message = "O prefixo do telefone não pode estar vazio")
+    @NotNull(message = "O prefixo do telefone não pode ser nulo")
     @Min(value = 10, message = "O prefixo do telefone deve conter 2 caracteres numéricos")
     @Max(value = 99, message = "O prefixo do telefone deve conter 2 caracteres numéricos")
     @Pattern(regexp = "\\d{2}", message = "O padrão do prefixo do telefone é inválido")
     private Integer prefixo;
 
-    @NotEmpty(message = "O número do telefone não pode estar vazio")
+    @NotNull(message = "O número do telefone não pode ser nulo")
     @Min(value = 8, message = "O número do telefone deve conter no mínimo {min} caracteres numéricos")
     @Min(value = 9, message = "O número do telefone deve conter no máximo {max} caracteres numéricos")
     @Pattern(regexp = "(\\d{9})|(\\d{8})", message = "O padrão do número do telefone é inválido")
