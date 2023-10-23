5ac773327a917ff39c506b513b53b148bb413996
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 09:32:35 -0300

    Feat: Adicionada anotação @ResponseStatus à exception

--- src/main/java/br/com/backend/exceptions/custom/TooManyRequestsException.java
+++ src/main/java/br/com/backend/exceptions/custom/TooManyRequestsException.java
@@ -1,5 +1,9 @@
 package br.com.backend.exceptions.custom;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.TOO_MANY_REQUESTS)
 public class TooManyRequestsException extends RuntimeException{
     public TooManyRequestsException(String message) {
         super(message);
