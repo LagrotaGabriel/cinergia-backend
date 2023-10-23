baae3999aa7e0dd75d96cb7546e7e0ea91269c1e
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 09:46:46 -0300

    Refac: - Validações do telefone ajustadas

--- src/main/java/br/com/backend/globals/models/telefone/request/TelefoneRequest.java
+++ src/main/java/br/com/backend/globals/models/telefone/request/TelefoneRequest.java
@@ -2,7 +2,9 @@ package br.com.backend.globals.models.telefone.request;
 
 import lombok.*;
 
-import javax.validation.constraints.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @Data
 @Builder
@@ -14,13 +16,11 @@ public class TelefoneRequest {
     @NotNull(message = "O prefixo do telefone não pode ser nulo")
     @Min(value = 10, message = "O prefixo do telefone deve conter 2 caracteres numéricos")
     @Max(value = 99, message = "O prefixo do telefone deve conter 2 caracteres numéricos")
-    @Pattern(regexp = "\\d{2}", message = "O padrão do prefixo do telefone é inválido")
     private Integer prefixo;
 
     @NotNull(message = "O número do telefone não pode ser nulo")
-    @Min(value = 8, message = "O número do telefone deve conter no mínimo {min} caracteres numéricos")
-    @Min(value = 9, message = "O número do telefone deve conter no máximo {max} caracteres numéricos")
-    @Pattern(regexp = "(\\d{9})|(\\d{8})", message = "O padrão do número do telefone é inválido")
+    @Min(value = 8, message = "O número do telefone deve conter no mínimo {value} caracteres numéricos")
+    @Min(value = 9, message = "O número do telefone deve conter no máximo {value} caracteres numéricos")
     private Integer numero;
 
     public String obtemPrefixoComNumeroJuntos() {
