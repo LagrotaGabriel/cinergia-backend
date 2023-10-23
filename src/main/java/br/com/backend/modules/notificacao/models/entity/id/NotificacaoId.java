4052761678f5e092a3908642ac420669676f36e8
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 09:51:50 -0300

    Refac: - Anotação @Type adicionada ao atributo uuidEmpresa

--- src/main/java/br/com/backend/modules/notificacao/models/entity/id/NotificacaoId.java
+++ src/main/java/br/com/backend/modules/notificacao/models/entity/id/NotificacaoId.java
@@ -1,16 +1,18 @@
 package br.com.backend.modules.notificacao.models.entity.id;
 
-import br.com.backend.modules.empresa.models.entity.EmpresaEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 
+import java.io.Serializable;
 import java.util.UUID;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class NotificacaoId {
+public class NotificacaoId implements Serializable {
+    @Type(type = "uuid-char")
+    private UUID empresa;
     private UUID uuid;
-    private EmpresaEntity empresa;
 }
