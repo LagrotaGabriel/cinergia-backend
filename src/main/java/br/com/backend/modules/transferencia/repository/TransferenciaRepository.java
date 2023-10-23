8066565270228c1504af32cec0e38a64f8525dc9
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 15:54:48 -0300

    Refac: - Layout das querys do TransferenciaRepository reajustado para melhorar a legibilidade do código

--- src/main/java/br/com/backend/modules/transferencia/repository/TransferenciaRepository.java
+++ src/main/java/br/com/backend/modules/transferencia/repository/TransferenciaRepository.java
@@ -1,6 +1,7 @@
 package br.com.backend.modules.transferencia.repository;
 
 import br.com.backend.modules.transferencia.models.entity.TransferenciaEntity;
+import br.com.backend.modules.transferencia.models.entity.id.TransferenciaId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,12 +9,14 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, Long> {
+public interface TransferenciaRepository extends JpaRepository<TransferenciaEntity, TransferenciaId> {
 
-    @Query("SELECT t FROM TransferenciaEntity t WHERE t.idEmpresaResponsavel = ?1")
-    Page<TransferenciaEntity> buscaPorTransferencias(Pageable pageable, Long id);
+    @Query("SELECT t FROM TransferenciaEntity t " +
+            "WHERE t.empresa.uuid = ?1")
+    Page<TransferenciaEntity> buscaPorTransferenciasPaginadas(Pageable pageable, UUID uuid);
 
     Optional<TransferenciaEntity> findByAsaasId(String asaasId);
 
