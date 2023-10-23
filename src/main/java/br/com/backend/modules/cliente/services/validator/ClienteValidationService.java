488ef56d299c40f159d1287fe7a7cd9ecfc8c66f
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 09:56:25 -0300

    Refac: - Como o nome das querys de validações no repositório foram alteradas, foi feita a alteração dos nomes destes novos métodos na classe de validações

--- src/main/java/br/com/backend/modules/cliente/services/validator/ClienteValidationService.java
+++ src/main/java/br/com/backend/modules/cliente/services/validator/ClienteValidationService.java
@@ -4,7 +4,6 @@ import br.com.backend.exceptions.custom.InvalidRequestException;
 import br.com.backend.modules.cliente.models.dto.request.ClienteRequest;
 import br.com.backend.modules.cliente.models.entity.ClienteEntity;
 import br.com.backend.modules.cliente.repository.ClienteRepository;
-import br.com.backend.modules.empresa.models.entity.EmpresaEntity;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,7 +20,7 @@ public class ClienteValidationService {
     public void validaSeCpfCnpjJaExiste(UUID uuidEmpresa, String cpfCnpj) {
         log.debug("Método de validação de chave única de CPF/CNPJ acessado");
 
-        if (Boolean.TRUE.equals(clienteRepository.existsByEmpresaUuidAndCpfCnpj(uuidEmpresa, cpfCnpj))) {
+        if (Boolean.TRUE.equals(clienteRepository.verificaSeClienteAtivoJaExisteComCpfCnpjInformado(uuidEmpresa, cpfCnpj))) {
             String mensagemErro = cpfCnpj.length() == 14
                     ? "O CPF informado já existe"
                     : "O CNPJ informado já existe";
@@ -34,7 +33,7 @@ public class ClienteValidationService {
     public void validaSeEmailJaExiste(UUID uuidEmpresa, String email) {
         log.debug("Método de validação de chave única de e-mail acessado");
 
-        if (Boolean.TRUE.equals(clienteRepository.existsByEmpresaUuidAndEmail(uuidEmpresa, email))) {
+        if (Boolean.TRUE.equals(clienteRepository.verificaSeClienteAtivoJaExisteComEmailInformado(uuidEmpresa, email))) {
             log.warn("O e-mail informado já existe: {}", email);
             throw new InvalidRequestException("O e-mail informado já existe");
         }
