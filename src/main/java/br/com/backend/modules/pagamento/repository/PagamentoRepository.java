c2cc528121f9d934a77ff5102fc950b04cc638b8
Gabriel <gabriellagrota23@gmail.com>
2023-10-23 09:45:27 -0300

    Feat: - Querys de buscas responsáveis por realizar cálculos de dashboards incrementados ao repository - Layout das querys ajustados melhorando a legibilidade das querys

--- src/main/java/br/com/backend/modules/pagamento/repository/PagamentoRepository.java
+++ src/main/java/br/com/backend/modules/pagamento/repository/PagamentoRepository.java
@@ -1,6 +1,7 @@
 package br.com.backend.modules.pagamento.repository;
 
 import br.com.backend.modules.pagamento.models.entity.PagamentoEntity;
+import br.com.backend.modules.pagamento.models.entity.id.PagamentoId;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,41 +10,72 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Repository
-public interface PagamentoRepository extends JpaRepository<PagamentoEntity, Long> {
-
-    @Query("SELECT SUM(p.valorBruto) FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1 and p.statusPagamento = 'ATRASADO'")
-    Double somaDePagamentosAtrasados(Long idEmpresa);
-
-    @Query("SELECT SUM(p.valorBruto) FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1 and p.statusPagamento = 'PENDENTE'")
-    Double somaDePagamentosPrevistos(Long idEmpresa);
-
-    @Query("SELECT SUM(p.valorBruto) FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1 and p.statusPagamento = 'APROVADO'")
-    Double somaDePagamentosConfirmados(Long idEmpresa);
+public interface PagamentoRepository extends JpaRepository<PagamentoEntity, PagamentoId> {
 
     @Query("SELECT p FROM PagamentoEntity p WHERE " +
-            "p.idEmpresaResponsavel = ?1 and p.idPlanoResponsavel = ?2")
-    Page<PagamentoEntity> buscaPorPagamentosDoPlano(Pageable pageable, Long idEmpresa, Long idPlano);
+            "p.empresa.uuid = ?1 and p.plano.uuid = ?2")
+    Page<PagamentoEntity> buscaPorPagamentosDoPlano(Pageable pageable, UUID uuidEmpresa, UUID uuidPlano);
 
     @Query("SELECT p FROM PagamentoEntity p WHERE " +
-            "p.idEmpresaResponsavel = ?1 and p.idClienteResponsavel = ?2")
-    Page<PagamentoEntity> buscaPorPagamentosDoCliente(Pageable pageable, Long idEmpresa, Long idCliente);
+            "p.empresa.uuid = ?1 and p.plano.cliente.uuid = ?2")
+    Page<PagamentoEntity> buscaPorPagamentosDoCliente(Pageable pageable, UUID uuidEmpresa, UUID uuidCliente);
 
-    @Query("SELECT p FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1 and p.statusPagamento = 'APROVADO'")
-    Page<PagamentoEntity> buscaPorPagamentosRealizados(Pageable pageable, Long id);
+    @Query("SELECT p FROM PagamentoEntity p WHERE p.empresa.uuid = ?1 and p.statusPagamento = 'APROVADO'")
+    Page<PagamentoEntity> buscaPorPagamentosRealizados(Pageable pageable, UUID uuidEmpresa);
 
-    @Query("SELECT p FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1")
-    Page<PagamentoEntity> buscaPorPagamentos(Pageable pageable, Long id);
+    @Query("SELECT p FROM PagamentoEntity p " +
+            "WHERE p.empresa.uuid = ?1 " +
+            "AND (?2 IS NULL OR p.descricao LIKE ?2%)")
+    Page<PagamentoEntity> buscaPaginadaPorPagamentos(Pageable pageable,
+                                                     UUID uuidEmpresa,
+                                                     String busca);
 
-    @Query("SELECT p FROM PagamentoEntity p WHERE p.idEmpresaResponsavel = ?1")
-    List<PagamentoEntity> buscaTodosPagamentosEmpresa(Long id);
+    @Query("SELECT p FROM PagamentoEntity p WHERE p.empresa.uuid = ?1")
+    List<PagamentoEntity> buscaTodosPagamentosEmpresa(UUID uuidEmpresa);
 
-    @Query("SELECT p FROM PagamentoEntity p WHERE upper(p.descricao) LIKE ?1% and p.idEmpresaResponsavel = ?2")
-    Page<PagamentoEntity> buscaPorPagamentosTypeAhead(Pageable pageable, String busca, Long id);
+    Optional<PagamentoEntity> findByAsaasId(String asaasId);
 
-    Optional<PagamentoEntity> findByIdAsaas(String asaasId);
+    @Query("SELECT COALESCE(SUM(p.valorBruto), 0) FROM PagamentoEntity p " +
+            "WHERE p.plano.uuid = ?1 " +
+            "AND p.plano.statusPlano <> 'REMOVIDO' " +
+            "AND p.statusPagamento <> 'CANCELADO'")
+    Double calculaTotalCobrancasDoPlano(UUID uuidPlano);
 
-    @Query("SELECT p FROM PagamentoEntity p WHERE p.id=?1 and p.idEmpresaResponsavel = ?2")
-    Optional<PagamentoEntity> buscaPorId(Long idPlano, Long idEmpresa);
+    @Query("SELECT COALESCE(SUM(p.valorBruto), 0) FROM PagamentoEntity p " +
+            "WHERE p.plano.uuid = ?1 " +
+            "AND p.plano.statusPlano <> 'REMOVIDO' " +
+            "AND p.statusPagamento = 'PENDENTE'")
+    Double calculaTotalCobrancasPendentesDoPlano(UUID uuidPlano);
+
+    @Query("SELECT COALESCE(SUM(p.valorBruto), 0) FROM PagamentoEntity p " +
+            "WHERE p.plano.uuid = ?1 " +
+            "AND p.plano.statusPlano <> 'REMOVIDO' " +
+            "AND p.statusPagamento = 'APROVADO'")
+    Double calculaTotalCobrancasAprovadasDoPlano(UUID uuidPlano);
+
+    @Query("SELECT COALESCE(SUM(p.valorBruto), 0) FROM PagamentoEntity p " +
+            "WHERE p.plano.uuid = ?1 " +
+            "AND p.plano.statusPlano <> 'REMOVIDO' " +
+            "AND p.statusPagamento = 'ATRASADO'")
+    Double calculaTotalCobrancasAtrasadasDoPlano(UUID uuidPlano);
+
+    @Query("SELECT COALESCE(COUNT(pgt), 0) FROM PagamentoEntity pgt " +
+            "WHERE pgt.plano.uuid = ?1 " +
+            "AND pgt.plano.statusPlano <> 'REMOVIDO' " +
+            "AND pgt.statusPagamento <> 'CANCELADO'")
+    Integer somaQuantidadeDeCobrancasDoPlano(UUID uuidPlano);
+
+    @Query("SELECT COALESCE(COUNT(p), 0) FROM PagamentoEntity p " +
+            "WHERE p.plano.uuid = ?1 " +
+            "AND p.plano.statusPlano <> 'REMOVIDO' " +
+            "AND p.statusPagamento = 'ATRASADO'")
+    Integer somaQuantidadeDeCobrancasAtrasadasDoPlano(UUID uuidPlano);
+
+    @Query("SELECT pgt FROM PagamentoEntity pgt " +
+            "WHERE pgt.dataVencimento < ?1 " +
+            "AND (pgt.statusPagamento != 'ATRASADO' AND pgt.statusPagamento != 'CANCELADO')")
+    List<PagamentoEntity> buscaPagamentosAtrasadosComStatusDiferenteDeAtrasadoECancelado(String hoje);
 }
